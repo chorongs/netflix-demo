a723befd7b78c4275b786a5fd41962edcd157641
MJ <charmblues@gmail.com>
2024-08-29 17:08:37 +0900

    Layout 완성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { Navbar } from 'react-bootstrap';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AppLayout from './layout/AppLayout';
+import HomePage from './pages/HomePage/HomePage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 
-// 홈페이지
-// 영화 전체 페이지
-// 영화 상세 페이지
+// 홈페이지  / 
+// 영화 전체 페이지 /movies
+// 영화 상세 페이지 /movies/:id
 
 
 // 각각 폴더로 구분하는 이유는 style을 각각 관리하기 위해서.
@@ -18,9 +22,14 @@ function App() {
 
     <Routes>
       <Route path='/' element={<AppLayout/>}>  
-      {/* // 이렇게 하게되면 Route 사이에 들어가는 모든 요소들은 AppLayout을 적용받는다. */}
-      
+          <Route index element={<HomePage />} />
+          <Route path='movies' >
+            <Route index element={<MoviePage />} />
+            <Route path=':id' element={<MovieDetail />} />
+          </Route>
       </Route> 
+
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet } from 'react-router-dom';
+import './AppLayout.style.css';
+
 
 const AppLayout = () => {
   return (
-    <div>navbar</div>
+    <div className='nav-back'>
+    <Navbar expand="lg" className="nav-bg">
+    <Container fluid>
+    <Navbar.Brand href="#" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <img
+              src="https://cdn.dribbble.com/users/9378043/screenshots/16832559/media/10b207c918d604662e088308d16b366d.png?resize=1600x1200&vertical=center"
+              alt="Netflix Logo"
+              width="135"
+              height="75"
+              className="d-inline-block align-top"
+              style={{ backgroundColor: 'transparent', border: 'none', display: 'block', marginRight:'20px' }}
+            />
+        {' '}
+        </Navbar.Brand>
+
+
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/movies">Link</Nav.Link>
+          
+        </Nav>
+        <Form className="search-box">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="dark">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+    <Outlet />
+    {/*  // 리액트에 새로나운 기술인데 router내부 자손을 가져올 수 있도록 도와준다. */}
+    </div>
+
+
   )
 }
 
