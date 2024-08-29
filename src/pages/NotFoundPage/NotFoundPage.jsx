530b4e65e9282b560e688a9ec709c21bc2c6dc47
MJ <charmblues@gmail.com>
2024-08-29 19:47:30 +0900

    404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1></h1>
+      <img 
+        src="https://blog.kakaocdn.net/dn/bVfy7b/btrBqDimNhi/YsGl5yhQPFSK55ooLExHA0/img.png"
+        alt="404 Image"
+        style={{ maxWidth: '100%', height: 'auto' }}
+      />
+      <p></p>
+    </div>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
