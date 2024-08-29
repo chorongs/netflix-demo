cd1ffe812c3d88651d287572e0aa62dd7f426789
MJ <charmblues@gmail.com>
2024-08-29 20:06:41 +0900

    404 page 완성

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 
 const NotFoundPage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1></h1>
-      <img 
-        src="https://blog.kakaocdn.net/dn/bVfy7b/btrBqDimNhi/YsGl5yhQPFSK55ooLExHA0/img.png"
-        alt="404 Image"
-        style={{ maxWidth: '100%', height: 'auto' }}
-      />
-      <p></p>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      textAlign: 'center',
+      backgroundColor: 'transparent', 
+      overflow: 'hidden' 
+    }}>
+      <a href="/" style={{ display: 'block' }}>
+        <img 
+          src="https://blog.kakaocdn.net/dn/bVfy7b/btrBqDimNhi/YsGl5yhQPFSK55ooLExHA0/img.png"
+          alt="404 Image"
+          style={{ 
+            width: '100%', 
+            height: 'auto', 
+            maxWidth: '1000px',
+          }}
+        />
+      </a>
     </div>
   );
 }
