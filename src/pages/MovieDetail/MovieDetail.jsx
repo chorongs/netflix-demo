5f7824c367122553a074f756d700588427313337
MJ <charmblues@gmail.com>
2024-09-06 15:33:27 +0900

    리뷰 마무리;

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Spinner, Alert, Container, Row, Col } from 'react-bootstrap';
 import useMovieDetailQuery from '../../hooks/useMovieDetail';
@@ -10,6 +10,35 @@ const MovieDetail = () => {
   const { data, isLoading, isError, error } = useMovieDetailQuery({ id });
   const { data: reviews, isLoading: isReviewsLoading, isError: isReviewsError, error: reviewsError } = useMovieReviewQuery(id);
 
+
+  const Review = ({review}) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const maxLength = 200;
+
+    const toggleReadMore = () => {
+      setIsExpanded(!isExpanded)
+    }
+
+    return (
+      <div className="review-box">
+        <h6>{review.author}</h6>
+        <p>
+          {isExpanded ? review.content : `${review.content.substring(0, maxLength)}...`}
+        </p>
+        {review.content.length > maxLength && (
+          <button onClick={toggleReadMore}>
+            {isExpanded ? '접기' : '더보기'}
+          </button>
+        )}
+
+      <p>
+        <small>{new Date(review.created_at).toLocaleDateString()}</small>
+      </p>
+      </div>
+    );
+  }
+
+  
 
   if (isLoading || isReviewsLoading) {
     return <Spinner animation="border" />;
@@ -67,17 +96,17 @@ const MovieDetail = () => {
           </div>
 
           <div>
-            <h1>리뷰박스 자리</h1>
+            <div className='review-name'>코멘트</div>
+            <div>
             {reviews && reviews.results.length > 0 ? (
              reviews.results.map(review => (
-            <div key={review.id} className="review-box">
-              <h5>{review.author}</h5>
-              <p>{review.content}</p>
+            <Review key={review.id}  review={review} />
+             ))
+            ) : (
+            <p>리뷰 사용이 불가능합니다.</p>
+            )}  
             </div>
-          ))
-        ) : (
-          <p>No reviews available</p>
-        )}  
+
           </div>
         </div>
   );
