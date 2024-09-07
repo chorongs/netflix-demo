8e43dca88190f1e4a58e135b9b087fdd4d89b1e0
MJ <charmblues@gmail.com>
2024-09-07 12:13:56 +0900

    비슷한 영화

--- src/pages/MovieDetail/components/SimilarMovieSlide/SimilarMovieSlide.jsx
+++ src/pages/MovieDetail/components/SimilarMovieSlide/SimilarMovieSlide.jsx
@@ -1,60 +1,60 @@
-// import React from 'react'
-// import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
-// import { Alert } from 'bootstrap';
-// import MovieCard from '../../../HomePage/components/MovieCard/MovieCard';
-// import "./SimilarMovieSlide.style.css"
-// import useSimilarMovieQuery from '../../../../hooks/useSimilarMovie';
-// import { useParams } from 'react-router-dom';
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { Alert } from 'bootstrap';
+import MovieCard from '../../../HomePage/components/MovieCard/MovieCard';
+import "./SimilarMovieSlide.style.css"
+import useSimilarMovieQuery from '../../../../hooks/useSimilarMovie';
+import { useParams } from 'react-router-dom';
 
-// const responsive = {
-//     desktop: {
-//       breakpoint: { max: 3000, min: 1024 },
-//       items: 8,
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 8,
 
-//     },
-//     tablet: {
-//       breakpoint: { max: 1024, min: 464 },
-//       items: 2,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
 
-//     },
-//     mobile: {
-//       breakpoint: { max: 464, min: 0 },
-//       items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
 
-//     }
-//   };
+    }
+  };
 
-// const SimilarMovieSlide = () => {
+const SimilarMovieSlide = () => {
 
-//   const {id} = useParams();
-//   const {data, isLoading, isError, error} = useSimilarMovieQuery(id)
+  const {id} = useParams();
+  const {data, isLoading, isError, error} = useSimilarMovieQuery(id)
 
-//   if (isLoading) {
-//     return <h1></h1>
-//   }
-//   if (isError) {
-//     return <Alert varient="danger">{error.message}</Alert>
-//   }
+  if (isLoading) {
+    return <h1></h1>
+  }
+  if (isError) {
+    return <Alert varient="danger">{error.message}</Alert>
+  }
 
-//   return (
-//     <div>
-//         <h3>비슷한 취향의 영화</h3>
+  return (
+    <div>
+        <h3>비슷한 취향의 영화</h3>
 
 
-//     <Carousel
-//     infinite={true}
-//     centerMode={true}
-//     itemClass='movie-slider p-1'
-//     containerClass='carousel-container'
-//     responsive={responsive}
-// >
-//     {data.results.map((movie,index)=> (
-//     <MovieCard movie={movie} key={index}/>
-//     ))}
-//     </Carousel>
-//     </div>
-//   )
-// }
+    <Carousel
+    infinite={true}
+    centerMode={true}
+    itemClass='movie-slider p-1'
+    containerClass='carousel-container'
+    responsive={responsive}
+>
+    {data.results.map((movie,index)=> (
+    <MovieCard movie={movie} key={index}/>
+    ))}
+    </Carousel>
+    </div>
+  )
+}
 
-// export default SimilarMovieSlide
+export default SimilarMovieSlide
