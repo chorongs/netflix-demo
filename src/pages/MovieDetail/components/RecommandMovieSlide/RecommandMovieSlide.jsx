cd8d4f67a18fffded8db891e3eca6b8e1c81586d
MJ <charmblues@gmail.com>
2024-09-07 13:26:18 +0900

    모바일 수정 완료

--- src/pages/MovieDetail/components/RecommandMovieSlide/RecommandMovieSlide.jsx
+++ src/pages/MovieDetail/components/RecommandMovieSlide/RecommandMovieSlide.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Alert } from 'bootstrap';
 import './RecommandMovieSlide.style.css';
 import useRecommandMovieQuery from '../../../../hooks/useRecommandMovie';
@@ -8,6 +8,26 @@ import { useParams } from 'react-router-dom';
 const RecommandMovieSlide = () => {
   const { id } = useParams();
   const { data, isLoading, isError, error } = useRecommandMovieQuery(id);
+
+  const [visibleMovies, setVisibleMovies] = useState(5);
+
+  const handleResize = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 767) {
+      setVisibleMovies(4); // 모바일에서는 4개만 표시
+    } else {
+      setVisibleMovies(5); // 기본 5개 표시
+    }
+  };
+
+  useEffect(() => {
+    handleResize(); // 초기 화면 크기에 따른 카드 수 설정
+    window.addEventListener('resize', handleResize); // 화면 크기 변경 시 카드 수 재설정
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // 이벤트 리스너 제거
+    };
+  }, []);
 
   if (isLoading) {
     return <h1>로딩 중...</h1>;
@@ -20,7 +40,7 @@ const RecommandMovieSlide = () => {
     <div>
       <h3>추천하는 영화</h3>
       <div className="recommand-movie-grid">
-        {data.results.slice(0, 5).map((movie, index) => (
+        {data.results.slice(0, visibleMovies).map((movie, index) => (
           <MovieCard movie={movie} key={index} />
         ))}
       </div>
